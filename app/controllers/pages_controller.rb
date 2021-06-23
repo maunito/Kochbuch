421a5da1743c0b219b0a82a3e30b22489e3cb632
Maurice Sattrasai <maurice.sattrasai@hotmail.com>
2021-06-23 11:28:28 +0200

    created route to impressum. Still have to do styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def home
     @recipes = Recipe.first(8)
   end
+
+  def impressum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  get "impressum", to: 'pages#impressum'
+
   get "recipes/:id", to: "recipes#show", as: :recipe
 
   resources :recipes do
